981cfac5f17dc03cc447e131db69e8472d121f96
radik <radik_@mail.ru>
2012-03-04 19:15:07 +0200

    Закончил главу 6.2

---
+++ db/migrate/20120304164641_add_email_uniqueness_index.rb
@@ -0,0 +1,6 @@
+class AddEmailUniquenessIndex < ActiveRecord::Migration
+  def change
+	add_index :users, :email, :unique => true
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,21 @@ describe User do
     User.create!(@attr)
   end
 
+
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should reject with duplicate email" do
+	User.create!(@attr)
+	upcase_email = @attr[:email].upcase
+	user_duplicate = User.new(@attr.merge(:email=>upcase_email))
+	user_duplicate.should_not be_valid
+  end
+
   it "should require a name"
 
 end
