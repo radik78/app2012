d03363db3a3cbf004260626a76e46c878eb79273
radik <radik_@mail.ru>
2012-03-09 19:38:24 +0200

    сделал создание и проверку безопасного пароля

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+# 
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	
+	attr_accessor :password	
+	attr_accessible :name, :email, :password, :password_confirmation, :encrypted_password
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, 
@@ -21,5 +24,47 @@ class User < ActiveRecord::Base
 		:presence    => true, 
 		:format      => { :with => email_regex },
 		:uniqueness  => { :case_sensitive => false}
+
+
+	# Automatically create the virtual attribute 'password_confirmation'.
+	validates :password,	:presence     => true,
+							:confirmation => true,
+							:length       => { :within => 6..40 }
+
+	before_save :make_encrypt_password
+
+		def identical_password?(submitted_password) 
+			encrypted_password == encrypt_algoritm(submitted_password)
+		end
+
+		def self.identificate (email, password)
+		#	finded_user = self.find_by_email(email)
+		#	correct_passw = finded_user.identical_password?(password)
+		#	if(finded_user && correct_passw)
+		#		return (finded_user)
+		#	else
+		#		return (nil)
+		#	end
+
+			user = find_by_email(email)
+			return nil  if user.nil?
+			return user if user.identical_password?(password)
+
+		end
+
+	private
+
+
+
+		def encrypt_algoritm(string)	
+			Digest::SHA2.hexdigest("#{string}--#{salt}")
+		end
+
+
+		def make_encrypt_password
+			self.salt = Digest::SHA2.hexdigest("#{Time.now.utc}--#{password}") if new_record?
+			self.encrypted_password = encrypt_algoritm(password)
+		end
+
 	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,40 +2,129 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
 
-  before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
-  end
+	#--------------------------------------------------------------------------------
 
-  it "should create a new instance given valid attributes" do
-    User.create!(@attr)
-  end
+	before(:each) do
+		@attr = {
+			 :name => "Example User",
+			 :email => "user@example.com",
+			 :password => "foobar",
+			 :password_confirmation => "foobar"
+		}
+	end
+
+
+ 	#--------------------------------------------------------------------------------
+
+	describe "name and email validation" do
+		it "should create a new instance given valid attributes" do
+			User.create!(@attr)
+		end
 
 
 
-  it "should reject names that are too long" do
-    long_name = "a" * 51
-    long_name_user = User.new(@attr.merge(:name => long_name))
-    long_name_user.should_not be_valid
-  end
+		it "should reject names that are too long" do
+			long_name = "a" * 51
+			long_name_user = User.new(@attr.merge(:name => long_name))
+			long_name_user.should_not be_valid
+		end
 
-  it "should reject with duplicate email" do
-	User.create!(@attr)
-	upcase_email = @attr[:email].upcase
-	user_duplicate = User.new(@attr.merge(:email=>upcase_email))
-	user_duplicate.should_not be_valid
-  end
+		it "should reject with duplicate email" do
+			User.create!(@attr)
+			upcase_email = @attr[:email].upcase
+			user_duplicate = User.new(@attr.merge(:email=>upcase_email))
+			user_duplicate.should_not be_valid
+		end
+	end
 
-  it "should require a name"
+
+	#--------------------------------------------------------------------------------
+
+
+	describe "password validation" do
+
+		it "should require a password" do
+			User.new(@attr.merge(:password => "", :password_confirmation => "")).
+			should_not be_valid
+		end
+	
+		it "should require a matching password confirmation" do
+			User.new(@attr.merge(:password_confirmation => "invalid")).
+			should_not be_valid
+		end
+	
+		it "should reject short passwords" do
+			short = "a" * 5
+			hash = @attr.merge(:password => short, :password_confirmation => short)
+			User.new(hash).should_not be_valid
+		end
+	
+		it "should reject long passwords" do
+			long = "a" * 41
+			hash = @attr.merge(:password => long, :password_confirmation => long)
+			User.new(hash).should_not be_valid
+		end
+		
+	end
+
+
+	#-----------------------------------------------------------------------------
+	describe "password encryption" do
+
+		before(:each) do
+			@user = User.create!(@attr)
+		end
+
+		it "should have an encrypted password attribute" do
+			@user.should respond_to(:encrypted_password)
+		end
+
+		it "should get true on correct submitted password" do
+			@user.identical_password?(@user.password).should be_true
+		end
+
+
+		it "should get false on incorrect submitted password" do
+			@user.identical_password?(@user.password+" ").should be_false
+		end
+
+
+		it "should set the encrypted password" do
+			@user.encrypted_password.should_not be_blank
+		end
+
+		describe "identificate method" do
+
+			it "should return object user on correct email and correct password" do
+				(User.identificate(@attr[:email], @attr[:password])==@user).should be_true
+			end
+			
+			it "should return nil on incorrect email" do
+				User.identificate("nonexist@com", @attr[:password]).should be_nil
+			end
+
+			it "should return nil on incorrect password" do
+				User.identificate(@attr[:email], "bad_pass").should be_nil
+			end
+
+
+		end
+
+	end
+
 
 end
+ 
+
