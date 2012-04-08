0088ecaefda91b39a4762c59fee430b35958819d
radik <radik_@mail.ru>
2012-04-08 19:58:00 +0300

    Finish 10.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,20 @@
 
 class UsersController < ApplicationController
 
+
+	#-------------------------------------------------------
     def show
 		@user = User.find(params[:id])
         @title = @user.name
     end
 
-
+	#-------------------------------------------------------
     def new
 		@user = User.new
         @title = 'Регистрация'
     end
 
+	#-------------------------------------------------------
 	def create
 		@user = User.new params[:user]
 		if @user.save
@@ -23,6 +26,26 @@ class UsersController < ApplicationController
 			@title = 'Регистрация'
 			render :new
 		end
+	end
+
+	#-------------------------------------------------------
+	def edit
+		@user = User.find(params[:id])
+        @title = "Редактирование"
+	end
+	#-------------------------------------------------------
+
+	def update
+		@user = User.find(params[:id])
+
+        if @user.update_attributes params[:user]
+			flash[:success]	= 'Данные успешно обновлены'
+			redirect_to user_path(@user)	
+		else
+			@title = 'Редактирование'
+			render 'edit'
+		end
+		
 	end
 
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe UsersController do
     render_views
 
+	#====================================================================	
 	describe "GET 'new'" do
 		
 		it "should be successful" do
@@ -19,6 +20,7 @@ describe UsersController do
 
 	end
 
+	#====================================================================
 	describe "GET 'show'" do
 
 		before(:each) do
@@ -56,8 +58,8 @@ describe UsersController do
 
 
 
-
-  describe "failure" do
+	#====================================================================
+	describe "failure create new user" do
 
       before(:each) do
         @attr = { :name => "", :email => "", :password => "",
@@ -80,50 +82,122 @@ describe UsersController do
         response.should render_template('new')
       end
   
-  end
+  	end
+
+
   
-  
-  describe "success" do
+	#====================================================================  
+	describe "success create new user" do
 	
-	before(:each) do
-		@attr = { 	:name 					=> "New User", 
-					:email 					=> "super@mail.ru",
-					:password 				=> "aaaaaa",
-					:password_confirmations => "aaaaaa"
-				}
+		before(:each) do
+			@attr = { 	:name 					=> "New User", 
+						:email 					=> "super@mail.ru",
+						:password 				=> "aaaaaa",
+						:password_confirmations => "aaaaaa"
+					}
+		end
+
+		#--- проверим что стало на одного пользователя больше при правильных параметрах ---
+		it "should create a user" do
+		    lambda do
+		      post :create, :user => @attr
+		    end.should change(User, :count).by(1)
+		end
+
+		#--- проверим, что после правильной регистрации нового пользователя переходим на страницу пользователя ----
+		it "should redirect to the user show page" do
+		    post :create, :user => @attr
+		    response.should redirect_to(user_path(assigns(:user)))
+		end
+
+		#--- проверим, что после правильной регистрации нового пользователя появляется FLASH сообщение ----
+		it "should have a welcome message" do
+			post :create, :user => @attr
+		    flash[:success].should =~ /Добро пожаловать/i
+		end
+
+ 		#--- проверим, что после правильной регистрации нового пользователя он будет в сессии
+		it "should sign the user in" do
+			post :create, :user=>@attr
+			controller.should be_signed_in
+		end
+
+
 	end
 
-	#--- проверим что стало на одного пользователя больше при правильных параметрах ---
-	it "should create a user" do
-        lambda do
-          post :create, :user => @attr
-        end.should change(User, :count).by(1)
-    end
+	#====================================================================
+	describe "GET 'edit'" do
 
-	#--- проверим, что после правильной регистрации нового пользователя переходим на страницу пользователя ----
-    it "should redirect to the user show page" do
-        post :create, :user => @attr
-        response.should redirect_to(user_path(assigns(:user)))
-    end
+		before(:each) do
+			@user = Factory(:user)
+			test_sign_in(@user)
+			get :edit, :id => @user
+		end
 
-	#--- проверим, что после правильной регистрации нового пользователя появляется FLASH сообщение ----
-	it "should have a welcome message" do
-        post :create, :user => @attr
-        flash[:success].should =~ /Добро пожаловать/i
-    end
 
- 	#--- проверим, что после правильной регистрации нового пользователя он будет в сессии
-	it "should sign the user in" do
-		post :create, :user=>@attr
-		controller.should be_signed_in
+		it "should be successful" do
+			response.should be_success
+		end
+
+		it "should have a rigth title" do
+			response.should have_selector("title", :content=>'Редактирование')
+		end
+
+		it "should have a link to change the Gravatar" do
+			response.should have_selector("a", :content=>'change')
+		end
+
 	end
 
 
-  end
+	#===================================================================
+	describe "PUT 'update' user" do
+
+		before(:each) do
+			@user = Factory(:user)
+			test_sign_in(@user)
+		end
+
+		describe "failure" do
+	
+			before(:each) do
+				@attr = {:name=>"", :email=>"", :password=>"", :confirm_password=>""}
+				put :update, :id=>@user, :user=>@attr
+			end
+	
+			it "should render the 'edit' page" do
+				response.should render_template 'edit'
+			end
+
+			it "should have right title" do
+				response.should have_selector 'title', :content=>'Редактирование'
+			end
+
+		end
+
+		describe "success" do
+			before(:each) do
+				@attr = { :name=>"New name", :email=>"new_email@mail.com", :password=>"qwertyu", :confirm_password=>"qwertyu"}
+				put :update, :id=>@user, :user=>@attr
+			end
+
+			it "should redirect the 'user' page" do
+				response.should redirect_to (user_path(@user))
+			end
+
+			it "should change the user's attributes" do
+				@user.reload
+				@user.name.should == @attr[:name]
+				@user.email.should == @attr[:email]
+			end
+			
+			it "should have a flash message" do
+				flash[:success].should =~ /Данные успешно обновлены/
+			end
 
 
-
-
+		end
+	end
 
 
 
